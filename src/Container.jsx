ee61261c5a345dcb934a14b093d5887d2bab631f
Cristian Gabbanini <cristian.gabbanini@gmail.com>
2019-01-02 17:05:02 +0100

    Adds a container component for theming

---
+++ src/Container.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const RequestContainer = styled.div``;
+
+function Container({ children }) {
+  return <RequestContainer>{children}</RequestContainer>;
+}
+
+export default Container;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Request from "./Request";
 import CustomerDetails from "./CustomerDetails";
 import Wizard from "./Wizard";
 import Confirmation from "./Confirmation";
+import Container from "./Container";
 
 import { ThemeProvider } from "emotion-theming";
 
@@ -17,27 +18,29 @@ const theme = {
 
 render(
   <ThemeProvider theme={theme}>
-    <Wizard
-      steps={[
-        "1 - Fill in your details",
-        "2 - Fill in your request",
-        "3 - Confirmation"
-      ]}
-    >
-      <CustomerDetails stepComplete={true} />
-      <Request
-        stepComplete={true}
-        minRooms={1}
-        maxRooms={5}
-        minAdults={1}
-        maxAdults={8}
-        minChildren={0}
-        maxChildren={5}
-        minChildrenAge={1}
-        maxChildrenAge={15}
-      />
-      <Confirmation />
-    </Wizard>
+    <Container>
+      <Wizard
+        steps={[
+          "1 - Fill in your details",
+          "2 - Fill in your request",
+          "3 - Confirmation"
+        ]}
+      >
+        <CustomerDetails stepComplete={true} />
+        <Request
+          stepComplete={true}
+          minRooms={1}
+          maxRooms={5}
+          minAdults={1}
+          maxAdults={8}
+          minChildren={0}
+          maxChildren={5}
+          minChildrenAge={1}
+          maxChildrenAge={15}
+        />
+        <Confirmation />
+      </Wizard>
+    </Container>
   </ThemeProvider>,
   document.getElementById("app")
 );
