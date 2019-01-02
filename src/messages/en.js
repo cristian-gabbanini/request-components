d3927e541f8a733c0997e44381c9fe155a881172
Cristian Gabbanini <cristian.gabbanini@gmail.com>
2019-01-02 18:24:04 +0100

    Adds the translation for month names and days of the week

--- src/messages/en.js
+++ src/messages/en.js
@@ -2,5 +2,28 @@ export default {
   quotation_request: "Quotation request",
   confirmation: "Request sent",
   rooms: "Room",
-  room: "Rooms"
+  room: "Rooms",
+  months: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "Semptember",
+    "October",
+    "November",
+    "Dicember"
+  ],
+  daysOfTheWeek: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ]
 };
